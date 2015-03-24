71a8debac851fe85ead1316606115832a2d600d0
Enrique Arias <enrique.arias.cervero@gmail.com>
2015-03-24 12:10:38 +0100

    Added shortcut 'rm' for 'site rm'

--- lib/mr_hyde/commands/remove.rb
+++ lib/mr_hyde/commands/remove.rb
@@ -6,7 +6,11 @@ module MrHyde
     class Remove < MrHyde::Command
       class << self
         def process(args, opts = {})
-          Site.remove args, opts
+          if args.length == 0
+          MrHyde.logger.warn("A site name must be typed. You can see a list of nested sites using the 'list' command.") 
+          else
+            Site.remove args, opts
+          end
         end
       end
     end
