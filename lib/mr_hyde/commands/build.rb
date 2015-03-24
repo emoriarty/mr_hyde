ad8ef98a34f0f8a86514d6abd6322c155242963f
Enrique Arias <enrique.arias.cervero@gmail.com>
2015-03-24 10:44:44 +0100

    Added 'build' shortcut for 'site build'

--- lib/mr_hyde/commands/build.rb
+++ lib/mr_hyde/commands/build.rb
@@ -13,7 +13,7 @@ module MrHyde
         # Options
         def process(args, opts = {})
           MrHyde::Site.build args, opts
-          MrHyde.logger.info "Built process is fisished, you can look the result in '#{MrHyde.destination}' folder"
+          MrHyde.logger.info "Built process is finished, you can see the result in '#{MrHyde.destination}' folder"
         end
       end
     end
